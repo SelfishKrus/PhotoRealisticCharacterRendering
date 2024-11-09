60416b7b7a7b7173b0ee578c6babf4ab5e8f4507
Krus_ROG_3060 <selfishkrus@github.com>
2024-11-09 18:31:57 +0800

    <lib> env lighting api

---
+++ Assets/Demo_PhotoRealisticCharacter/Script/Runtime/Shader/GetPbrParams.hlsl
@@ -0,0 +1,19 @@
+﻿// Surface 
+float distanceFromFragToCam = distance(IN.posWS, _WorldSpaceCameraPos);
+_S_Roughness = lerp(_S_Roughness, 1.0, distanceFromFragToCam / _DetailVisibleDistance);
+
+ShadingSurface surf = GetShadingSurface(_BaseColorMap, _BaseColorTint, _S_Opacity, _T_Rmom, float3(_S_Roughness, _S_Metallic, _S_AO), _T_Normal, _S_Normal, IN.normalWS, _T_DetailNormal, _S_DetailNormal, _DetailNormalTiling, _DetailVisibleDistance, distanceFromFragToCam, IN.tangentWS, SamplerState_Linear_Repeat, IN.uv);
+
+float2 posSS = IN.pos.xy / _ScreenParams.xy;
+
+// Shading Variables
+DirectionalLightData lightData = _DirectionalLightDatas[0];
+ShadingInputs si = GetShadingInputs(surf.normalWS_high, IN.posWS, lightData, _WrapLighting);
+HDShadowContext shadowContext = InitShadowContext();
+#if defined(RECEIVE_DIRECTIONAL_SHADOW)
+    float shadow = GetDirectionalShadowAttenuation(shadowContext,
+					    posSS, IN.posWS, surf.normalWS_geom,
+					    lightData.shadowIndex, si.L);
+#else 
+    float shadow = 1;
+#endif
--- Assets/Demo_PhotoRealisticCharacter/Script/Runtime/Shader/K_Lighting.hlsl
+++ Assets/Demo_PhotoRealisticCharacter/Script/Runtime/Shader/K_Lighting.hlsl
@@ -2,7 +2,35 @@
 #define K_LIGHTING_INCLUDED
 
     // DL - Directional Light 
+    
+    #include "Packages/com.unity.render-pipelines.high-definition/Runtime/Material/BuiltinGIUtilities.hlsl"
 
     #include "K_BxDF.hlsl"
+    #include "K_ShadingInputs.hlsl"
+    #include "K_ShadingSurface.hlsl"
+
+    float3 EvaluateEnvDiffuse(ShadingSurface surf)
+    {   
+        float3 irradiance_SH = EvaluateLightProbe(surf.normalWS_geom);
+        return surf.baseColor * irradiance_SH;
+    }
+
+    float3 EvaluateEnvSpecular(ShadingSurface surf, ShadingInputs si)
+    {   
+        float3 brdf = EnvBRDF(surf.F0, surf.roughness, si.NoV);
+
+        float3 reflectDir = reflect(-si.V, surf.normalWS_detail);
+        float3 irradiance_IBL = SampleSkyTexture(reflectDir, surf.envMipLevel, 0).rgb;
+
+        return brdf * irradiance_IBL;
+    }
+
+    float3 EvaluateEnvLighting(ShadingSurface surf, ShadingInputs si)
+    {
+        float3 diffuse = EvaluateEnvDiffuse(surf);
+        float3 specular = EvaluateEnvSpecular(surf, si);
+
+        return diffuse + specular;
+    }
 
 #endif 
